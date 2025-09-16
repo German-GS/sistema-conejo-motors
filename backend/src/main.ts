bbb0a64774846483ae82c9e82228927b4c5c1186
German García siles <germangarciasiles@192.168.1.13>
2025-09-16 15:58:47 -0600

    Correccion de erres y aplicacion de sistema mejorado de captura de errores

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter'; // <-- 1. Importar el filtro
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +13,11 @@ async function bootstrap() {
     prefix: '/uploads/',
   });
   app.useGlobalPipes(new ValidationPipe());
+  
+  // --- 👇 LÍNEA AÑADIDA 👇 ---
+  app.useGlobalFilters(new AllExceptionsFilter()); // <-- 2. Registrar el filtro globalmente
+  // -------------------------
+
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
